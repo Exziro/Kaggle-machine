35056b813146b305d53121663448d1b828326d76
yixi qi <qiyixi2010@Gmail.com>
2017-10-11 23:19:40 +0800

    分类集

---
+++ 朴素贝叶斯/朴素贝叶斯.py
@@ -0,0 +1,11 @@
+def createVocabList(dataSet):
+    """
+    获取所有单词的集合
+    :param dataSet: 数据集
+    :return: 所有单词的集合(即不含重复元素的单词列表)
+    """
+    vocabSet = set([])  # create empty set
+    for document in dataSet:
+        # 操作符 | 用于求两个集合的并集
+        vocabSet = vocabSet | set(document)  # union of the two sets
+    return list(vocabSet)
