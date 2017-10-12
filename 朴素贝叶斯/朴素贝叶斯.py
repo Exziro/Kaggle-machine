6b5df848a41342df7da30b90ce298fc4e0c8e013
yixi qi <qiyixi2010@Gmail.com>
2017-10-12 23:20:48 +0800

    构建词分类

--- 朴素贝叶斯/朴素贝叶斯.py
+++ 朴素贝叶斯/朴素贝叶斯.py
@@ -9,3 +9,19 @@ def createVocabList(dataSet):
         # 操作符 | 用于求两个集合的并集
         vocabSet = vocabSet | set(document)  # union of the two sets
     return list(vocabSet)
+def setOfWords2Vec(vocabList, inputSet):
+    """
+    遍历查看该单词是否出现，出现该单词则将该单词置1
+    :param vocabList: 所有单词集合列表
+    :param inputSet: 输入数据集
+    :return: 匹配列表[0,1,0,1...]，其中 1与0 表示词汇表中的单词是否出现在输入的数据集中
+    """
+    # 创建一个和词汇表等长的向量，并将其元素都设置为0
+    returnVec = [0] * len(vocabList)# [0,0......]
+    # 遍历文档中的所有单词，如果出现了词汇表中的单词，则将输出的文档向量中的对应值设为1
+    for word in inputSet:
+        if word in vocabList:
+            returnVec[vocabList.index(word)] = 1
+        else:
+            print "the word: %s is not in my Vocabulary!" % word
+    return returnVec
